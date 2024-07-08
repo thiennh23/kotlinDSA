8a603204968850a78d66505c8b5c41ad8c4db56c
thiennh23 <21522626@gm.uit.edu.vn>
2024-07-08 08:27:54 +0700

    Code force

---
+++ src/codeforce_test.kt
@@ -0,0 +1,13 @@
+fun main() {
+    print("Enter the name: ")
+    var name = readlnOrNull()
+    println(name)
+}
+
+fun logic(name: String): Boolean {
+    /*var arr = arrayOf("");
+    for (i in name.length) {
+        arr[i] = name[i]
+    }*/
+    return true
+}
