e4bcd83233fd3252f3bd86e2eec6ca9ab83cc23f
thiennh23 <21522626@gm.uit.edu.vn>
2024-07-09 08:03:48 +0700

    Test

--- src/codeforce_test.kt
+++ src/codeforce_test.kt
@@ -1,13 +1,29 @@
-fun main() {
-    print("Enter the name: ")
-    var name = readlnOrNull()
-    println(name)
-}
+//fun main() {
+//    print("Enter the name: ")
+//    var name = readlnOrNull()
+//    println(name)
+//}
+//
+//fun logic(name: String): Boolean {
+//    /*var arr = arrayOf("");
+//    for (i in name.length) {
+//        arr[i] = name[i]
+//    }*/
+//    return true
+//}
 
-fun logic(name: String): Boolean {
-    /*var arr = arrayOf("");
-    for (i in name.length) {
-        arr[i] = name[i]
+fun main() {
+    val x: Any = 29.9
+
+    /*when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
     }*/
-    return true
+
+    var a: String? = "ABC"
+    println(a)
+    a = null
+    println(a)
 }
